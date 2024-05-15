a2644cff4b6129c917f7749ba1edd950aea16213
MNBT3112 <162482142+MNBT3112@users.noreply.github.com>
2024-05-15 16:40:12 +0000

    ok go ahead

--- src/script.js
+++ src/script.js
@@ -1,28 +1,30 @@
 'use strict';
 
 
-
-
 const navbar = document.querySelector(".navbar");
 
 const logo1 = document.querySelector("#logo1");
+const banner_logo = document.getElementById("banner_logo");
+
+
 
 
 window.addEventListener("scroll" , function(){
-  
+   
+        
+   
 navbar.classList.toggle('sticky', window.scrollY>0);
 
-if(this.window.scrollY>0){
-logo1.src = "LOGO2.png"
-}
-else{
-    logo1.src = "Logo-trans.png"
-}
 
 
 
 
 })
+
+
+
+
+
 
 
 
